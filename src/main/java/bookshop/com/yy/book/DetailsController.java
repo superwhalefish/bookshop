f95b54d12dd40666c2d02b57207c744bda858616
superwhalefish <50001859+superwhalefish@users.noreply.github.com>
2019-09-24 11:35:11 +0800

    whale:details

--- src/main/java/bookshop/com/yy/book/DetailsController.java
+++ src/main/java/bookshop/com/yy/book/DetailsController.java
@@ -1,5 +1,13 @@
 package bookshop.com.yy.book;
 
-public class DetailsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class DetailsController {
+	@GetMapping("/details")
+	public String details() {
+		return "details";
+
+	}
 }
